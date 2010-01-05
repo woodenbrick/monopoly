f371b6aaec972230586978bf32993b4e125d688c
Daniel Woodhouse <wodemoneke@gmail.com>
2010-01-05 00:25:06 +0000

    created cardstack class

---
+++ monopoly/cardstack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import random
+
+class CardStack(object):
+    def __init__(self, card_data):
+        self.deck = []
+        self.used_cards = []
+        for card in card_data:
+            self.deck.append(Card(card))
+        random.shuffle(self.deck)
+    
+    def pickup(self, player):
+        card = self.deck.pop(0)
+        if card.is_gojfc:
+            player.gojfc.append(card)
+        else:
+            self.used_cards.append(card)
+        #when we reach the end of the deck we will shuffle
+        if len(self.deck) == 0:
+            self.deck, self.used_cards = self.used_cards, self.deck
+            random.shuffle(self.deck)
+    
+    def return_gojfc(self, player):
+        self.used_cards.append(player.gojfc.pop())
+        
