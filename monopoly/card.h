249af430149bbb5c7630d121dc34dd8581a0aa5c
Daniel Woodhouse <wodemoneke@gmail.com>
2010-01-22 19:08:51 +0000

    updated Card header

--- monopoly/card.h
+++ monopoly/card.h
@@ -1,4 +1,35 @@
 #ifndef CARD_H
 #define CARD_H
 
+#include <QString>
+#include "player.h"
+
+class CardStack
+{
+    Card *deck[];
+    Card *usedCards[];
+    shuffle();
+
+public:
+    CardStack(QString cardData[][]);
+    void pickup(Player *player);
+    void returnGojfc(Card card);
+
+};
+
+
+class Card
+{
+    int id;
+    QString text;
+    bool isGojfc;
+    int streetRepairs; //set to 0 if not street repairs otherwise it is the amount per house
+    int money; //a payment to be made (- pay, + receive) or the cost of street repairs per hotel
+    bool playerTransfer; //true if money should be transfered between players
+    int square; //a square to move to or -1 if not moving.
+
+public:
+    Card(QString cardData[]);
+};
+
 #endif // CARD_H
