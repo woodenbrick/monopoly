d07fe6901da2b8501aaff40ce650f8983a89b36f
Daniel Woodhouse <wodemoneke@gmail.com>
2010-02-01 18:43:01 +0000

    added board class

--- monopoly/board.cpp
+++ monopoly/board.cpp
@@ -1,5 +1,6 @@
 #include "board.h"
-
+#include "stdio.h"
+#include "printf.h"
 
 //    std::vector<Square> square;
 //    CardStack *communityChest;
@@ -7,53 +8,147 @@
 //std::vector<Player> players;
 
 
-Board::Board(QString locale)
+Settings::Settings(int cash, int goMoney, int superTax, QString currencySymbol)
 {
-    conn.Open("resources/db/" + locale + ".sqlite");
+    this->cash = cash;
+    this->goMoney = goMoney;
+    this->superTax = superTax;
+    this->currencySymbol = currencySymbol;
+}
+
+Board::Board(QString locale, std::vector<std::vector<QString> > namesAndImages)
+{
+    QString database = "resources/db/";
+    database.append(locale).append(".sqlite");
+    std::string db = std::string(database.toLatin1());
+    conn.Open(db);
+    statement = conn.Statement("select cash, go, super, currency from settings");
+    QString currency(statement->ValueString(3).c_str());
+    settings = new Settings(statement->ValueInt(0), statement->ValueInt(1), statement->ValueInt(2), currency);
     communityChest = createCardStack("community_chest");
     chance = createCardStack("chance");
-    squares = createSquares();
+    squareFactory();
+
+    //get Go square for players to start on
+    Square &go = getSquare(0);
+    std::vector<std::vector<QString> >::iterator it;
+    Player * playerPtr;
+    for(it = namesAndImages.begin(); it < namesAndImages.end(); it++)
+        playerPtr = new Player(QString("Daniel"), go, settings->getCash(), QString("Hat.png"));
+        players.push_back(playerPtr);
+
 
 }
 
 CardStack *Board::createCardStack(QString type)
 {
-    CardStack stack;
-    statement = conn.Statement("select * from ?");
-    statement->Bind(type);
+    CardStack *stack = new CardStack;
+    bool pl, goj;
+    statement = conn.Statement("select id, text, isGojfc, streetRepairs, money, playerTrans, square, from ?");
+    std::string t = std::string(type.toLatin1());
+    statement->Bind(0, t);
     while (statement->NextRow())
     {
-        Card card = Card(statement->ValueInt(0), statement->ValueString(1), statement->ValueInt(2),
-                         statement->ValueInt(3), statement->ValueInt(4), statement->ValueInt(5), statement->ValueInt(6));
-        stack.addToStack(card);
+        (statement->ValueInt(2) == 1) ? goj = true : goj = false;
+        (statement->ValueInt(5) == 1) ? pl = true : pl = false;
+        Card *card = new Card(statement->ValueInt(0), QString(statement->ValueString(1).c_str()), goj,
+                         statement->ValueInt(3), statement->ValueInt(4), pl, statement->ValueInt(6));
+        stack->addToStack(card);
     }
-    return * stack;
+    return stack;
 
 }
 
-std::vector <Square> *Board::createSquares()
+void Board::squareFactory()
 {
-    // unfinished
-    statement = conn.Statement("select * from squares");
+    statement = conn.Statement(
+            "select id, name, price, _set, street.rent, street.one, street.two, street.three, street.four, "
+            "street.five from squares LEFT OUTER JOIN street ON squares.id = street.square_id");
+    Square *sqrPtr;
+    std::vector<int> rents;
+    //_set is one of None|B|LB|P|O|R|Y|G|DB|RR|UT
+    while(statement->NextRow())
+    {
+        QString name = QString(statement->ValueString(1).c_str());
+        QString set = QString(statement->ValueString(3).c_str());
+        if(statement->ValueString(2) == "None")
+        {
+            sqrPtr = new Square(statement->ValueInt(0), name, set);
+        }
+
+        else if(statement->ValueString(2) == "RR")
+        {
+            sqrPtr = new Railway(statement->ValueInt(0), name, statement->ValueInt(2),
+                              set, statement->ValueInt(4));
+        }
+        else if(statement->ValueString(2) == "UT")
+        {
+            sqrPtr = new Utility(statement->ValueInt(0), name, statement->ValueInt(2),
+                               set, statement->ValueInt(4));
+        }
+        else
+        {
+             rents.clear();
+             for(int i=4; i<10; i++)
+             {
+                 rents.push_back(statement->ValueInt(i));;
+             }
+             sqrPtr = new Street(statement->ValueInt(0), name, statement->ValueInt(2),
+                                set, rents);
+        }
+        squares.push_back(sqrPtr);
+    }
+    //give references to each street about other houses in its set
+    for(int i=0; i<squares.size(); i++)
+    {
+        if(squares.at(i)->isStreet())
+        {
+            squares.at(i)->setOthersInSet(getSet(squares.at(i)));
+        }
+    }
+    //give references to each player about the other players
 
 }
 
-Square Board::getSquare(int id)
+Square& Board::getSquare(int id)
 {
+    return *squares.at(id);
+//    std::vector<Square*>::iterator it;
+//    for(it=squares.begin(); it<squares.end(); it++)
+//    {
+//        if(it->getId() == id)
+//        {
+//            return &it;
+//        }
+//    }
+}
+
+HouseSet* Board::getSet(Square* thisStreet)
+{
+    HouseSet * houseset;
+    std::vector<Square *> streetPtr;
+    for(int i=0; i<squares.size(); i++)
+    {
+        if(squares.at(i)->getSet() == thisStreet->getSet() && thisStreet->isEqual(squares.at(i)))
+        {
+            streetPtr.push_back(squares.at(i));
+        }
+    }
+    houseset = new HouseSet(streetPtr);
+    return houseset;
 
 }
 
-HouseSet Board::getSet(int id)
+std::vector<Player*>* Board::getOtherPlayers(Player *player)
 {
-
-}
-
-HouseSet Board::getSet(Property property)
-{
-
-}
-
-std::vector<Player> *returnOtherPlayers(Player *player)
-{
+    std::vector<Player*> *otherPlayers;
+    for(int i=0; i<players.size(); i++)
+    {
+        if(&players.at(i) != &player)
+        {
+            otherPlayers->push_back(players.at(i));
+        }
+    }
+    return otherPlayers;
 
 }
--- monopoly/board.h
+++ monopoly/board.h
@@ -8,23 +8,39 @@
 #include "player.h"
 #include "lib/sqlitewrapper.h"
 
+class Settings
+{
+    int cash;
+    int goMoney; //also income tax
+    int superTax;
+    QString currencySymbol;
+
+public:
+    Settings(int cash, int goMoney, int superTax, QString currencySymbol);
+    int getCash();
+    int getGoMoney();
+    int getSuperTax();
+    QString getCurrencySymbol();
+};
+
 class Board
 {
-    std::vector<Square> square;
+    std::vector<Square*> squares;
+    std::vector<Player*> players;
     CardStack *communityChest;
     CardStack *chance;
     SQLiteWrapper conn;
     SQLiteStatement *statement;
+    Settings *settings;
 
 
 public:
-    std::vector<Player> players;
     CardStack *createCardStack(QString type);
-    Board(QString locale);
-    Square getSquare(int id);
-    HouseSet getSet(int id);
-    HouseSet getSet(Property property);
-    std::vector<Player> *returnOtherPlayers(Player *player);
+    Board(QString locale, std::vector<std::vector<QString> > namesAndImages);
+    Square& getSquare(int id);
+    HouseSet* getSet(Square *thisStreet);
+    void squareFactory();
+    std::vector<Player*>* getOtherPlayers(Player *player);
 };
 
 
