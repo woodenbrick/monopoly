ba08ac621d2e828d7d9e21b27ec9da54d86a59b7
Daniel Woodhouse <wodemoneke@gmail.com>
2010-02-05 15:55:06 +0000

    added new files

---
+++ monopoly/offer.h
@@ -0,0 +1,12 @@
+#ifndef OFFER_H
+#define OFFER_H
+
+class Player;
+
+class Offer
+{
+    Offer(Player *player2, int payment, int propertyIds[], int paymentPlayer2, int propertyIdsPlayer2[]);
+};
+
+
+#endif // OFFER_H
