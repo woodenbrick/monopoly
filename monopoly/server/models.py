2d9a9f3a67e529e9b30059c8ecfb5fa0b97981bd
Daniel Woodhouse <wodemoneke@gmail.com>
2010-02-16 14:49:03 +0000

    continued python webserver

--- monopoly/server/models.py
+++ monopoly/server/models.py
@@ -2,12 +2,20 @@
 
 from google.appengine.ext import db
 
+class User(db.Model):
+    name = db.StringProperty()
+    password = db.StringProperty()
+    email = db.EmailProperty()
+    
+
 class NewGame(db.Model):
-    game_id = db.IntegerProperty()
-    creator = db.UserProperty()
+    game_id = db.StringProperty()
+    creator = db.ReferenceProperty(User)
     ip = db.StringProperty()
+    status = db.StringProperty()
     locale = db.StringProperty()
-    current_players = db.IntegerProperty()
+    current_player_count = db.IntegerProperty()
+    current_player_names = db.StringListProperty()
     max_players = db.IntegerProperty()
     min_players = db.IntegerProperty()
     
--- monopoly/server/server.py
+++ monopoly/server/server.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python
 import time
-from google.appengine.ext.webapp import RequestHandler
+from google.appengine.ext.webapp import RequestHandler, WSGIApplication
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
-from google.appengine.ext.webapp.template import render
+from google.appengine.ext.webapp.template import render 
 import models
 
+class NewUser(RequestHandler):
+    def post(self):
+        user = models.User(name=self.request.get("name"),
+                    password=self.request.get("password"),
+                    email=self.request.get("email"))
+        user.put()
+        
 class CreateGame(RequestHandler):
     def post(self):
-        creator = users.User(self.request.get("email"))
-        ip = self.request.remote_addr
+        creator = models.User.all().filter("name =", self.request.get("name")).get()
         #game id based on creator/time
-        game_id = email.nickname() + "_" + round(time.time())
-        min_players = self.request.get("min")
-        max_players = self.request.get("max")
-        game = models.NewGame(game_id=game_id, creator=creator, ip=ip, current_players=1,
-                       min_players=min_players, max_players=max_players)
+        game_id = creator.name + "_" + str(round(time.time()))
+        min_players = int(self.request.get("min"))
+        max_players = int(self.request.get("max"))
+        locale = self.request.get("locale")
+        game = models.NewGame(game_id=game_id, creator=creator, ip=self.request.remote_addr,
+                              locale=locale, current_player_count=1, current_player_names=[creator.name],
+                       status="available", min_players=min_players, max_players=max_players)
         game.put()
         
 class ListGames(RequestHandler):
     def get(self):
-        pass
+        games = models.NewGame.all().filter("status =", "available").fetch(100)
+        self.response.headers['Content-type'] = "text/xml"
+        self.response.out.write(render("templates/gamelist.xml", {"games" : games}))
+        
     
 class JoinGame(RequestHandler):
     def post(self):
-        pass
-    
+        game = models.NewGame.all().filter("game_id =", self.request.get("game_id")).get()
+        user = self.request.get("name")
+        game.current_player_names.append(user)
+        game.current_player_count += 1
+        game.put()
+        
 class LeaveGame(RequestHandler):
     def post(self):
-        pass
+        game = models.NewGame.all().filter("game_id =", self.request.get("game_id")).get()
+        user = self.request.get("name")
+        game.current_player_names.remove(user)
     
 class SaveGame(RequestHandler):
     def post(self):
@@ -37,3 +54,16 @@ class SaveGame(RequestHandler):
 class LoadGame(RequestHandler):
     def get(self):
         pass
+    
+
+app = WSGIApplication(
+    [("/game/create", CreateGame),
+    ("/game/list", ListGames),
+    ("/game/leave", LeaveGame),
+    ("/game/join", JoinGame),
+    ("/user/create", NewUser)
+    ],
+    debug=True
+    
+)
+run_wsgi_app(app)
