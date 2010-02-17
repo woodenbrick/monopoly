3cbd950cb39635eac9c73b9fb3d0a5c25851b66f
Daniel Woodhouse <wodemoneke@gmail.com>
2010-02-17 00:10:22 +0000

    split gui building into seperate classes

--- monopoly/main.cpp
+++ monopoly/main.cpp
@@ -1,22 +1,28 @@
-#include "gui/ui_board.h"
 #include <QApplication>
+#include <vector>
+#include <iostream>
+
 #include "player.h"
 #include "card.h"
 #include "square.h"
 #include "board.h"
-#include <vector>
-#include <iostream>
-int main(int argc, char *argv[])
+#include "gui/lobby.h"
+#include "gui/boardgui.h"
+
+int main(int argc, char* argv[])
 {
     //variables that should be built on the fly
     //we will hard code them here for testing purposes
+    QApplication app(argc, argv);
     QString locale = "UK";
 
-    QApplication app(argc, argv);
-    QMainWindow* win = new QMainWindow();
-    Ui::MainWindow gui;
-    gui.setupUi(win);
-    win->show();
+    //create windows
+    BoardGui *boardgui = new BoardGui(locale);
+    boardgui->show();
+
+    Lobby *lobby= new Lobby;
+    lobby->show();
+
     std::vector<std::vector<QString> > players;
     std::vector<QString> player;
     player.push_back("Daniel");
@@ -26,9 +32,6 @@ int main(int argc, char *argv[])
     player.push_back("Lewis");
     player.push_back("Dog");
     Board *board = new Board(locale, players);
-    QPixmap bpic(":/images/locale/UK/board.png");
-    gui.board_pic->setPixmap(bpic);
-    gui.board_pic->show();
     int total = board->getCurrentPlayer()->dice->roll();
     std::cout << total;
     std::cout << board->getCurrentPlayer()->dice->getLastRollString().toStdString();
